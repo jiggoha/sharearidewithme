a5d2686e52abcbeeb2d7a761c28ca4f162872344
Jiaao Hou <jiaao.hou@yale.edu>
2014-10-04 18:31:20 -0400

    This should work - sharing cab

--- app/controllers/ride_controller.rb
+++ app/controllers/ride_controller.rb
@@ -30,9 +30,12 @@ class RideController < ApplicationController
 				#if we can improve the cost for both users splitting the costs
 			  if change_route?(d, d.users[0], @new_user)[0] 
 			  	#remember the cost for new user
-			  	costs_newuser[driver.id] = change_route?(d, d.users[0], @new_user)[3]
+			  	costs_newuser[driver.id] = change_route?(d, d.users[0], @new_user)[1]
 			  	#and remember who gets off first, what is the cost for the old user
+			  	#[user1_lowerNewCost, whoGetsOffFirst, whoGetsOffSecond]
 			  	costs_infoaboutcab[driver.id] = change_route?(d, d.users[0], @new_user)[2..4]
+			  else
+			  	costs_newuser[driver.id] = change_route?(d, d.users[0], @new_user)[1]
 			  end	
 
 		  else
@@ -42,7 +45,18 @@ class RideController < ApplicationController
 
 		@estimated_price = costs.values.min
 		@saved_cost = costs.values.max - @estimated_price
+		#if we save anything (this means that we share the cab)
 		if @save_cost > 0
+			#if the driver has not yet picked up the guy you share with, set route
+			if(driver.route.length > 1)
+			  driver.route = [d.users[0].start_location, @new_user.start_location, costs_infoaboutcab[1].end_location, costs_infoaboutcab[2].end_location]
+			 #if the driver has already picked up the guy we share with
+		  elsif (driver.route.length == 1)
+        driver.route = [@new_user.start_location, costs_infoaboutcab[1].end_location, costs_infoaboutcab[2].end_location]
+    #if we are not sharing
+		else
+			driver.route = [@new_user.start_location, @new_user.end_location]
+		end
       
 		end
 	end
--- app/helpers/ride_helper.rb
+++ app/helpers/ride_helper.rb
@@ -37,7 +37,7 @@ module RideHelper
     end
 
     if (user1_currentcost > user1_lowerNewCost) and (user2_alonecost > user2_lowerNewCost)
-      [true, user1_newcost, user2_lowerNewCost, whoGetsOffFirst, whoGetsOffSecond]
+      [true, user2_lowerNewCost, user1_lowerNewCost, whoGetsOffFirst, whoGetsOffSecond]
     else
       [false, user1_currentcost]
     end
