e58b77dcf721077f4bb8bcdb3f6b226722fa2fa1
Evan Walsh <walsh45@purdue.edu>
2014-10-04 16:40:28 -0400

    Began adding pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+	def home
+  end
+  def about
+  end	
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
