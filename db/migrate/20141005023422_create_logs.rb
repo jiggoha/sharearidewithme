f6b74fe398177f7c5dcf2f596c24a1cd56a00380
Ebstein <stanislaw.swid@gmail.com>
2014-10-04 22:36:02 -0400

    created model

--- db/migrate/20141005023422_create_logs.rb
+++ db/migrate/20141005023422_create_logs.rb
@@ -1,10 +1,8 @@
 class CreateLogs < ActiveRecord::Migration
   def change
     create_table :logs do |t|
-    	t.string :tag
-    	t.text :message
 
-    	t.timestamps
+      t.timestamps
     end
   end
 end
